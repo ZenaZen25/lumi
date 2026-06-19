8952b8a3c8ff891d7043ae3d84a250418dbbd514
ZenaZen25 <Misslamis11@gmail.com>
2026-06-19 09:24:34 +0200

    correction profilController

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Badge;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ELEVE');
 
@@ -18,7 +20,22 @@ class ProfilController extends AbstractController
         $totalPoints = 0;
 
         foreach ($user->getCouragePoints() as $point) {
-            $totalPoints += $point->getPoints();
+            $totalPoints += $point->getPoints() ?? 0;
+        }
+
+        $nextBadge = null;
+        $pointsNeeded = 0;
+
+        $allBadges = $em->getRepository(Badge::class)->findBy([], [
+            'pointsRequis' => 'ASC',
+        ]);
+
+        foreach ($allBadges as $badge) {
+            if ($badge->getPointsRequis() !== null && $badge->getPointsRequis() > $totalPoints) {
+                $nextBadge = $badge;
+                $pointsNeeded = $badge->getPointsRequis() - $totalPoints;
+                break;
+            }
         }
 
         return $this->render('profil/index.html.twig', [
@@ -26,6 +43,8 @@ class ProfilController extends AbstractController
             'totalPoints' => $totalPoints,
             'points' => $user->getCouragePoints(),
             'badges' => $user->getUserBadges(),
+            'nextBadge' => $nextBadge,
+            'pointsNeeded' => $pointsNeeded,
         ]);
     }
 }
