c0a6eb205668823f98053e4b50e4e1a2ba9f9a64
ZenaZen25 <Misslamis11@gmail.com>
2026-07-10 16:50:39 +0200

    modification partiel, ajoute = cookies ajouté

---
+++ assets/controllers/cookie_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["banner"];
+
+//   connect() {
+//     if (!localStorage.getItem("lumi_rgpd")) {
+//       this.bannerTarget.classList.remove("hidden");
+//       document.body.classList.add("overflow-hidden");
+//     }
+//   }
+connect() {
+    this.bannerTarget.classList.remove("hidden");
+}
+
+  accept() {
+    localStorage.setItem("lumi_rgpd", "accepted");
+    this.bannerTarget.classList.add("hidden");
+    document.body.classList.remove("overflow-hidden");
+  }
+
+  refuse() {
+    localStorage.removeItem("lumi_rgpd");
+    window.location.href = "/";
+  }
+}
